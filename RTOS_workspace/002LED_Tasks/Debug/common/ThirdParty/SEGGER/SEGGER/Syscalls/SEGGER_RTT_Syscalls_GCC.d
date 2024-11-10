efa85c765346a4491d4a3fc07eb3009384fac4d3
andrea <andrea.fato20@gmail.com>
2024-11-10 16:52:01 +0100

    Added LED_Task002

---
+++ RTOS_workspace/002LED_Tasks/Debug/common/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+common/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
