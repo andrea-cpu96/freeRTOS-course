bd2a7874945bb1d14bdced903df8a8f218a48635
andrea <andrea.fato20@gmail.com>
2024-11-10 14:51:08 +0100

    Added task01 to ws

---
+++ RTOS_workspace/001Tasks/Debug/ThirdParty/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,36 @@
+ThirdParty/FreeRTOS/portable/MemMang/heap_4.o: \
+ ../ThirdParty/FreeRTOS/portable/MemMang/heap_4.c \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/Global.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/Global.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h:
---
+++ RTOS_workspace/001Tasks/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
