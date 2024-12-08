792449c7e50d1a9271b3d4456f5cb3e596fa6e94
andrea <andrea.fato20@gmail.com>
2024-12-08 15:30:54 +0100

    Add queues example

---
+++ RTOS_workspace/007Queues_n_timers/Debug/Core/Src/led_effect.d
@@ -0,0 +1,102 @@
+Core/Src/led_effect.o: ../Core/Src/led_effect.c ../Core/Inc/main.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Core/Inc/stm32f4xx_hal_conf.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/Config/Global.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/portable.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/list.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/queue.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h \
+ D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/timers.h
+../Core/Inc/main.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Core/Inc/stm32f4xx_hal_conf.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/Config/Global.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/portable.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/list.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/queue.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/task.h:
+D:/Andrea/Embedded/repositories/freeRTOS\ course/RTOS_workspace/common/ThirdParty/FreeRTOS/include/timers.h:
